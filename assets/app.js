1746b209cd9f967e331a8f3961df7fb9c3719a3a
Fatima <123065883+fatima-masmas@users.noreply.github.com>
2023-03-29 07:01:55 +0200

    Roting

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,16 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// start the Stimulus application
-import './bootstrap';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        hello 
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
+
